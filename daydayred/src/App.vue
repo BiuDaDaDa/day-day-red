078d6a2bdb47792ac5f127f58b12f0896c791ace
Your Name <you@example.com>
2017-11-16 16:08:50 +0800

    docs:重新上传最终版脚手架

--- daydayred/src/App.vue
+++ daydayred/src/App.vue
@@ -5,26 +5,9 @@
 </template>
 <script>
 export default {
-  name: 'app',
-  methods: {
-    fetchHomeData () {
-      this.$request({
-        url: '/api/news/banner',
-        type: 'get',
-        headers: {},
-        params: {},
-        success: function (res) {
-          console.log(res)
-        },
-        failed: function () {
-        }
-      })
-    }
-  }
+  name: 'app'
 }
 </script>
 <style scoped lang="less">
-  #app{
-  }
 
 </style>
