ed8531c6766a2b3af4b1cd0f6a91ebadc25969d6
Your Name <you@example.com>
2017-11-16 17:49:28 +0800

    dosc:2.3

--- daydayred/src/router/index.js
+++ daydayred/src/router/index.js
@@ -6,6 +6,7 @@ import ZhaoJiang from './ZhaoJiang'
 import YuKaiRui from './YuKariRui'
 Vue.use(Router)
 let routes = YuKaiRui.concat(OuXueSong, WeiShiBo, ZhaoJiang)
+console.log(routes)
 export default new Router({
   mode: 'history',
   routes: routes
