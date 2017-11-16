05198301a7cbe56766cecdea97827c70fb512ac8
Your Name <you@example.com>
2017-11-16 11:51:43 +0800

    docs:配置脚手架

---
+++ daydayred/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  methods: {
+    fetchHomeData () {
+      this.$request({
+        url: '',
+        type: 'get',
+        headers: {},
+        params: {},
+        success: function () {
+        },
+        failed: function () {
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+  @color:red;
+  @import '../node_modules/marx-css/css/marx.min.css';
+
+</style>
