45702711574a2f63c5f91217600da026ada5839b
Your Name <you@example.com>
2017-11-17 09:36:35 +0800

    chore:2.4

---
+++ daydayred/src/components/login.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <!--登陆注册容器-->
+    <div class="login-wrap">
+      <!--账号登陆-->
+      <div class="login-user">
+        <p class="login-user-dl">
+          账号登陆
+        </p>
+        <p class="login-user-yzm">
+          验证码登陆
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Login'
+  }
+</script>
+
+<style scoped lang="less">
+  @import "../common/css/style.less";
+  .login-wrap{
+    width: 100%;
+    height: 100%;
+    position: relative;
+  }
+  .login-user{
+    width: 60%;
+    position: absolute;
+    left: 30%;
+    top: 10vmin;
+    display: flex;
+    background-color: #fff;
+    border-radius: 1.06667vmin;
+    overflow: hidden;
+    min-height: 8vmin;
+    opacity: 1;
+  }
+</style>
--- daydayred/src/router/YuKariRui/index.js
+++ daydayred/src/router/YuKariRui/index.js
@@ -1,8 +1,14 @@
 import User from '@/components/tth-User/User'
+import Login from '@/components/Login'
 export default [
   {
     path: '/user',
     name: 'User',
     component: User
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
